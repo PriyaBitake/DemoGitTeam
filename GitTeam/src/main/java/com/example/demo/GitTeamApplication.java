7f86ed11f421069b958393c09de396f8182cec83
PriyaBitake <priyaBitake21@gmail.com>
2023-09-12 15:37:17 +0530

    First Draft Of the Project

---
+++ GitTeam/src/main/java/com/example/demo/GitTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTeamApplication.class, args);
+	}
+
+}
